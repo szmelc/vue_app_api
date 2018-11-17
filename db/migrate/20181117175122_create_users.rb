8752faf8d7909536b6840969003a65836fa12a85
Lukasz Szmelc <lszmelc@gmail.com>
2018-11-17 18:54:54 +0100

    Add users table

--- db/migrate/20181117175122_create_users.rb
+++ db/migrate/20181117175122_create_users.rb
@@ -1,9 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :password_digest
-      t.boolean :admin
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.boolean :admin, default: false
 
       t.timestamps
     end

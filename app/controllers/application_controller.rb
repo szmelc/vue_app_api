f6a654fbf7366b68ec20ad277ad4b86a315a6e60
Lukasz Szmelc <lszmelc@gmail.com>
2018-11-21 20:08:42 +0100

    Refactor ApllicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,33 @@
 class ApplicationController < ActionController::Base
-  include TokenAuthenticatable
-  include AdminAuthorizable
+  module AdminAuthorizable
+    extend ActiveSupport::Concern
+
+    included do
+      rescue_from NotPermittedException, with: -> { render json: { error: 'Not Permitted' }, status: :forbidden }
+    end
+
+    def authorize!(action)
+      raise NotPermittedException if action != :read && !current_user.admin?
+      true
+    end
+  end
+
+  module TokenAuthenticatable
+    extend ActiveSupport::Concern
+
+    included do
+      attr_reader :current_user
+
+      before_action :authenticate_user
+
+      rescue_from NotAuthorizedException, with: -> { render json: { error: 'Not authorized' }, status: :unauthorized }
+    end
+
+    private
+
+    def authenticate_user
+      @current_user = DecodeAuthenticationCommand.call(request.headers).result
+      raise NotAuthorizedException unless @current_user
+    end
+  end  
 end

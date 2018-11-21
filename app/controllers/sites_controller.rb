5452faa6baa1cb8d8181563ea12197d57d9491c1
Lukasz Szmelc <lszmelc@gmail.com>
2018-11-21 23:48:48 +0100

    Add and configure ActivAdmin gem

---
+++ app/controllers/sites_controller.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+class SitesController < ApplicationController
+  respond_to :html
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: "sites#index"
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   mount GrapeSwaggerRails::Engine => '/docs'
   mount API::Base, at: "/"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html

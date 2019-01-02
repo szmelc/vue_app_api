49eb4b74b1add9989db832978a9d6d2d6835c5fa
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-01-02 23:41:48 +0100

    Add BaseUploader

---
+++ app/uploaders/base_uploader.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class BaseUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+end

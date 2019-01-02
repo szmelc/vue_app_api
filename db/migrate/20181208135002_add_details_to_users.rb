7823c9e87e06805c33a8719eb9f560f10f284dd6
Łukasz Szmelc <lukasz.szmelc@netguru.pl>
2019-01-02 23:40:21 +0100

    Add details to users

--- db/migrate/20181208135002_add_details_to_users.rb
+++ db/migrate/20181208135002_add_details_to_users.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 class AddDetailsToUsers < ActiveRecord::Migration[5.2]
   def change
-    add_column :users, :first_name, :string, null: false
-    add_column :users, :last_name, :string, null: false
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
     add_column :users, :description, :text
   end
 end
